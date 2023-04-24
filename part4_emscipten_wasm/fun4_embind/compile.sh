0274c7e7f5569efc9fee119dc717381c6772c3b2
Oleksiy Grechnyev <shrike4625@gmail.com>
2023-04-24 16:55:46 +0300

    update fun4

--- part4_emscipten_wasm/fun4_embind/compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-emcc -o myadd.js myadd.cpp -O3 -lembind
--- part4_emscipten_wasm/fun4_embind/myadd.cpp
+++ part4_emscipten_wasm/fun4_embind/myadd.cpp
@@ -2,10 +2,12 @@
 
 #include <emscripten/bind.h>
 
+// Add two numbers
 int int_add(int x, int y) {
     return x + y;
 }
 
+// Now we can concatenate two std::strings !
 std::string str_add(const std::string &s1, const std::string &s2) {
     return s1 + s2;
 }
@@ -13,6 +15,7 @@ std::string str_add(const std::string &s1, const std::string &s2) {
 
 using namespace emscripten;
 
+// Generate bindings for our two functions
 EMSCRIPTEN_BINDINGS(my_module) {
     function("int_add", &int_add);
     function("str_add", &str_add);
