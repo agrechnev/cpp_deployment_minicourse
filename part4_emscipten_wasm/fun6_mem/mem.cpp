bfa26f3338a384977f1a3bb015046de21c95af05
Oleksiy Grechnyev <shrike4625@gmail.com>
2023-04-25 14:50:57 +0300

    fun6

---
+++ part4_emscipten_wasm/fun6_mem/mem.cpp
@@ -0,0 +1,56 @@
+// Fun 6: Check how much memory we can allocate
+
+#include <cstdlib>
+#include <iostream>
+
+#include <emscripten.h>
+
+extern "C" {
+
+    EMSCRIPTEN_KEEPALIVE void mem_fun() {
+        using namespace std;
+        
+        
+        // Emscripten is 32-bit, size_t and pointers are 32-bit
+        // 4 Gb is the limit !
+        cout << "void* : " << sizeof(void*) << endl;
+        cout << "size_t : " << sizeof(size_t) << endl;
+        cout << "int : " << sizeof(int) << endl;
+        cout << "long : " << sizeof(long) << endl;
+        cout << "long long : " << sizeof(long long) << endl;
+        
+        // Try allocation up to 4GB with malloc
+        for (size_t i=1; i<= 10; ++i) {
+            size_t m = i * 400;  // In Mib
+            cout << "malloc  " << m << " MiB  : " << (m * 1024 * 1024) << " bytes ";
+            void * p = malloc(m * 1024 * 1024);
+            if (p == nullptr) {
+                cout << "failed !" << endl;
+                break;
+            } else {
+                cout << "success" << endl;
+                free(p);
+            }
+        }
+        
+        // Same with new
+        for (size_t i=1; i<= 10; ++i) {
+            size_t m = i * 400;  // In Mib
+            cout << "new  " << m << " MiB  : " << (m * 1024 * 1024) << " bytes ";
+            
+            char * p = new char[m * 1024 * 1024];
+            if (p == nullptr) {
+                cout << "failed !" << endl;
+                break;
+            } else {
+                cout << "success" << endl;
+                delete [] p;
+            }
+        }
+        
+        
+        // Alocate 2GB and leave it allocated
+        malloc(2u * 1024 * 1024 * 1024);
+    }
+
+}
