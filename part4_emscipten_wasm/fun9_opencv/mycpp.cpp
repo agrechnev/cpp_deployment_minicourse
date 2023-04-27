7000e83abdae2a42299040d307f97f8500cea3db
Oleksiy Grechnyev <shrike4625@gmail.com>
2023-04-27 16:11:48 +0300

    fun9 in progress

--- part4_emscipten_wasm/fun9_opencv/mycpp.cpp
+++ part4_emscipten_wasm/fun9_opencv/mycpp.cpp
@@ -12,11 +12,29 @@
 extern "C" {
     
     
-    EMSCRIPTEN_KEEPALIVE void cpp_fun() {
+    EMSCRIPTEN_KEEPALIVE
+    void processImage(uint8_t *buffer, size_t size, int width, int height){
         using namespace std;
+        using namespace cv;
         
-        cv::Mat m = cv::Mat::ones(23, 34, CV_8UC3);
-        cout << m.size << endl;
+        cout << "OpenCV version = " << CV_VERSION << endl;
+        CV_Assert(4 * width*height == size);
+        
+        // Wrap buffer with a cv::Mat (no copying)
+        Mat m1(height, width, CV_8UC4, buffer);
+        
+        // Convert RGBA->BGR
+        Mat m2;
+        cvtColor(m1, m2, COLOR_RGBA2BGR);
+        
+        // Modify the BGR image with OpenCV
+        int w13 = width / 3;
+        int h13 = height / 3;
+        Mat m(m2, Range(h13, 2*h13), Range(w13, 2*w13));
+        bitwise_not(m, m);
+        
+        // Convert BGR->GRBA
+        cvtColor(m2, m1, COLOR_BGR2RGBA);
         
     }
     
